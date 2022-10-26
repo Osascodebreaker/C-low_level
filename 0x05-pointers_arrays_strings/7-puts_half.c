9190b87afb95561a6d328f8df2fcdfa0e21b3a9b
Osascodebreaker <victoraitanun3@gmail.com>
2022-10-25 17:20:10 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * * puts_half - prints a string, followed by a new line,
- * * @str: pointer to the string to print
- * * Return: void
- * */
+ * puts_half - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+ */
 
 void puts_half(char *str)
 {
